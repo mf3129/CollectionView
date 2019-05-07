10f0a655d9162c7372e77de8f3e90d6fd1db3732
Makan Fofana <mfofanaT99@gmail.com>
2019-05-07 19:38:09 -0400

    Addition of  iconSet data and cell datasource and dequeue of cell.

--- Controller/IconCollectionViewController.swift
+++ Controller/IconCollectionViewController.swift
@@ -12,45 +12,56 @@ private let reuseIdentifier = "Cell"
 
 class IconCollectionViewController: UICollectionViewController {
 
+    private var iconSet: [Icon] = [ Icon(name: "candle", price: 3.99, isFeatured: false),
+                                    Icon(name: "cat", price: 2.99, isFeatured: true),
+                                    Icon(name: "dribbble", price: 1.99, isFeatured: false),
+                                    Icon(name: "ghost", price: 4.99, isFeatured: false),
+                                    Icon(name: "hat", price: 2.99, isFeatured: false),
+                                    Icon(name: "owl", price: 5.99, isFeatured: true),
+                                    Icon(name: "pot", price: 1.99, isFeatured: false),
+                                    Icon(name: "pumkin", price: 0.99, isFeatured: false),
+                                    Icon(name: "rip", price: 7.99, isFeatured: false),
+                                    Icon(name: "skull", price: 8.99, isFeatured: false),
+                                    Icon(name: "sky", price: 0.99, isFeatured: false),
+                                    Icon(name: "toxic", price: 2.99, isFeatured: false),
+                                    Icon(name: "ic_book", price: 2.99, isFeatured: false),
+                                    Icon(name: "ic_backpack", price: 3.99, isFeatured: false),
+                                    Icon(name: "ic_camera", price: 4.99, isFeatured: false),
+                                    Icon(name: "ic_coffee", price: 3.99, isFeatured: true),
+                                    Icon(name: "ic_glasses", price: 3.99, isFeatured: false),
+                                    Icon(name: "ic_ice_cream", price: 4.99, isFeatured: false),
+                                    Icon(name: "ic_smoking_pipe", price: 6.99, isFeatured: false),
+                                    Icon(name: "ic_vespa", price: 9.99, isFeatured: false)]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return iconSet.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! IconCollectionViewCell
+        
         // Configure the cell
+        let icon = iconSet[indexPath.row]
+        cell.iconImageView.image = UIImage(named: icon.name)
+        cell.iconPriceLabel.text = "$\(icon.price)"
+        cell.backgroundView = (icon.isFeatured) ? UIImageView(image: UIImage(named: "feature-bg")) : nil
+        
     
         return cell
     }
